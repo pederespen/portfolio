daa2693dc7e4ed143186b0b44818178827739f0b
pespen <pederespen@gmail.com>
2025-04-03 14:59:48 +0200

    fixed some colors, added favicon

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -47,7 +47,7 @@ export const Contact = () => {
             <div className="text-left">
               <button
                 type="submit"
-                className="inline-block px-6 py-3 bg-blue-800 text-blue-100 font-bold rounded-md hover:bg-blue-200 transition-colors mt-4 w-auto cursor-pointer"
+                className="inline-block px-6 py-3 bg-gray-700 text-white font-bold rounded-md hover:bg-gray-800 transition-colors mt-4 w-auto cursor-pointer"
               >
                 {t("contact.send")}
               </button>
