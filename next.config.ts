17c1f7f61b25633e06d79744c2a5a249df71946c
pespen <pederespen@gmail.com>
2025-04-04 18:55:52 +0200

    add github deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
   // Enable importing JSON files
   webpack(config) {
     config.module.rules.push({
