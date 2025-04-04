afe5f3fcdeace4700c9f881521722d4c84626633
pespen <pederespen@gmail.com>
2025-04-04 19:07:07 +0200

    fix image not loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: "export",
+  // Configure images to work with static export
+  images: {
+    unoptimized: true,
+  },
   // Enable importing JSON files
   webpack(config) {
     config.module.rules.push({
